997c87e06e3962b66baf97e22022d0b10ea89bbe
IrdiZ <67896863+IrdiZ@users.noreply.github.com>
2024-07-10 20:02:34 +0200

    added fs for data analysis

---
+++ server/logic/getOpenDataEncapsulatedDataItems.js
@@ -0,0 +1,117 @@
+const { Client } = require("pg");
+const fs = require("fs");
+require("dotenv").config({
+  path: __dirname + "/../../.env",
+}); // This loads the environment variables from .env into process.env
+const https = require("https");
+function clientConnection() {
+  const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "postgres",
+    password: process.env.SECRET_KEY,
+    port: 5432,
+  });
+
+  return new Promise((resolve, reject) => {
+    client
+      .connect()
+      .then(() => {
+        console.log("Connected to PostgreSQL!");
+        resolve(client); // Resolve the client instance once connected
+      })
+      .catch((reason) => {
+        console.error("Error connecting to PostgreSQL ...", reason);
+        reject(reason); // Reject with the reason if connection fails
+      });
+  });
+}
+
+clientConnection()
+  .then((client) => {
+    // Use the connected client for queries
+    return client.query("SELECT slug, id from maindataitem;");
+  })
+  .then((res) => {
+    console.log("Query result:", res.rows);
+    console.log(" " + res.rows.length);
+    getDataItemPerSlug(res.rows)
+      .then((allData) => {
+        console.log("All data retrieved, its length is", allData.length);
+        // Process allData as needed
+        //write to file
+        fs.writeFile("output.json", JSON.stringify(allData), "utf8", (err) => {
+          if (err) {
+            console.error("Error writing file:", err);
+            return;
+          }
+          console.log("File has been saved successfully.");
+        });
+      })
+      .catch((error) => {
+        console.error("Error retrieving data:", error);
+      });
+  })
+  .catch((error) => {
+    console.error("Error during query:", error);
+  });
+
+//? Nice improvement here, Promise.all significantly reduced fetching time
+//? as opposed to before attempt using just simple iterative fetching
+//? from 10-20s to around 1s
+/*
+ * @returns {JSON} returns the slug info as json obj
+ */
+async function getDataItemPerSlug(slugObjectArray) {
+  try {
+    const fetchPromises = slugObjectArray.map(async (slugObject) => {
+      try {
+        const data = await fetchFromCustomUrl(slugObject.slug);
+        console.log(`Data retrieved for slug ${slugObject.slug}`);
+
+        jsonobject = JSON.parse(data);
+        return jsonobject;
+      } catch (error) {
+        console.error(
+          `Error fetching data for slug ${slugObject.slug}:`,
+          error
+        );
+        return null; // or handle the error as needed
+      }
+    });
+
+    const allObjectList = await Promise.all(fetchPromises);
+    return allObjectList.filter((data) => data !== null);
+  } catch (error) {
+    console.error("Error in getDataItemPerSlug:", error);
+    return []; // or handle the error as needed
+  }
+}
+
+function fetchFromCustomUrl(slug) {
+  return new Promise((resolve, reject) => {
+    let finalData = "";
+    https
+      .get(
+        {
+          hostname: "opendata.gov.al",
+          agent: false, // Create a new agent just for this one request,
+          path: "/api/sq/services/" + slug,
+        },
+        (res) => {
+          // Handle response data
+          res.on("data", (d) => {
+            finalData += d;
+          });
+
+          // Resolve finalData when response ends
+          res.on("end", () => {
+            resolve(finalData);
+          });
+        }
+      )
+      .on("error", (e) => {
+        reject(e); // Reject promise on error
+      });
+  });
+}
