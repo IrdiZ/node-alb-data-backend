15e647c35e4b46bf4fa0bb8d90bc8202e76c3d03
IrdiZ <67896863+IrdiZ@users.noreply.github.com>
2024-07-11 03:07:02 +0200

    2 tables working now, 1 more :)

--- server/logic/getOpenDataEncapsulatedDataItems.js
+++ server/logic/getOpenDataEncapsulatedDataItems.js
@@ -4,15 +4,15 @@ require("dotenv").config({
   path: __dirname + "/../../.env",
 }); // This loads the environment variables from .env into process.env
 const https = require("https");
-function clientConnection() {
-  const client = new Client({
-    user: "postgres",
-    host: "localhost",
-    database: "postgres",
-    password: process.env.SECRET_KEY,
-    port: 5432,
-  });
 
+const client = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: process.env.SECRET_KEY,
+  port: 5432,
+});
+function clientConnection() {
   return new Promise((resolve, reject) => {
     client
       .connect()
@@ -38,15 +38,55 @@ clientConnection()
     getDataItemPerSlug(res.rows)
       .then((allData) => {
         console.log("All data retrieved, its length is", allData.length);
-        // Process allData as needed
-        //write to file
-        fs.writeFile("output.json", JSON.stringify(allData), "utf8", (err) => {
-          if (err) {
-            console.error("Error writing file:", err);
-            return;
-          }
-          console.log("File has been saved successfully.");
-        });
+
+        //for each object
+        for (const object of allData) {
+          console.log(object);
+          //TODO get content and columns into maindataitem
+
+          const serviceDataList = object.serviceDataList; // assuming serviceDataList is part of object
+          // get id
+          getIdByTitle(object.name)
+            .then((result) => {
+              console.log("Result:", result);
+              // Further processing of the result here
+              // this means all serviceDataCategoryIds are null
+              const insertPromises = serviceDataList.map((el) => {
+                const customJsonEntry = {
+                  dataitemid: result[0].id, // get it from main item,
+                  name: el.name,
+                  url: el.url,
+                  type: el.type,
+                  icon: el.icon,
+                  html: el.html,
+                  slug: el.slug,
+                  downloadNumber: el.downloadNumber,
+                  serviceDataCategoryId: el.serviceDataCategoryId,
+                };
+
+                return insertServiceDataListItem(customJsonEntry);
+              });
+
+              return Promise.all(insertPromises);
+            })
+            .then(() => {
+              console.log("All entries inserted successfully.");
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+            });
+        }
+
+        //TODO now we handle serviceDataCategoryList
+
+        // //write to file
+        // fs.writeFile("output.json", JSON.stringify(allData), "utf8", (err) => {
+        //   if (err) {
+        //     console.error("Error writing file:", err);
+        //     return;
+        //   }
+        //   console.log("File has been saved successfully.");
+        // });
       })
       .catch((error) => {
         console.error("Error retrieving data:", error);
@@ -55,6 +95,58 @@ clientConnection()
   .catch((error) => {
     console.error("Error during query:", error);
   });
+
+function insertServiceDataListItem(entry) {
+  return new Promise((resolve, reject) => {
+    const queryText = `
+        INSERT INTO serviceDataListItem 
+        (dataitemid, name, url, type, icon, html, slug, downloadNumber, serviceDataCategoryId) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+      `;
+    const values = [
+      entry.dataitemid,
+      entry.name,
+      entry.url,
+      entry.type,
+      entry.icon,
+      entry.html,
+      entry.slug,
+      entry.downloadNumber,
+      entry.serviceDataCategoryId,
+    ];
+
+    client
+      .query(queryText, values)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject("Error executing insert query", err);
+      });
+  });
+}
+
+function getIdByTitle(title) {
+  return new Promise((resolve, reject) => {
+    if (!client._connected) {
+      client.connect().catch((err) => {
+        return reject("Error connecting to PostgreSQL:", err);
+      });
+    }
+
+    const queryText = "SELECT id FROM maindataitem WHERE title = $1";
+    const values = [title];
+
+    client
+      .query(queryText, values)
+      .then((res) => {
+        resolve(res.rows);
+      })
+      .catch((err) => {
+        reject("Error executing query", err);
+      });
+  });
+}
 
 //? Nice improvement here, Promise.all significantly reduced fetching time
 //? as opposed to before attempt using just simple iterative fetching
