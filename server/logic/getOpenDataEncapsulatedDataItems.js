0a1f6ac9c2df045bf667882d3004c6663a629eda
IrdiZ <67896863+IrdiZ@users.noreply.github.com>
2024-07-11 16:11:28 +0200

    Able to get full data into DB from DS

--- server/logic/getOpenDataEncapsulatedDataItems.js
+++ server/logic/getOpenDataEncapsulatedDataItems.js
@@ -12,6 +12,7 @@ const client = new Client({
   password: process.env.SECRET_KEY,
   port: 5432,
 });
+
 function clientConnection() {
   return new Promise((resolve, reject) => {
     client
@@ -26,76 +27,6 @@ function clientConnection() {
       });
   });
 }
-
-clientConnection()
-  .then((client) => {
-    // Use the connected client for queries
-    return client.query("SELECT slug, id from maindataitem;");
-  })
-  .then((res) => {
-    console.log("Query result:", res.rows);
-    console.log(" " + res.rows.length);
-    getDataItemPerSlug(res.rows)
-      .then((allData) => {
-        console.log("All data retrieved, its length is", allData.length);
-
-        //for each object
-        for (const object of allData) {
-          console.log(object);
-          //TODO get content and columns into maindataitem
-
-          const serviceDataList = object.serviceDataList; // assuming serviceDataList is part of object
-          // get id
-          getIdByTitle(object.name)
-            .then((result) => {
-              console.log("Result:", result);
-              // Further processing of the result here
-              // this means all serviceDataCategoryIds are null
-              const insertPromises = serviceDataList.map((el) => {
-                const customJsonEntry = {
-                  dataitemid: result[0].id, // get it from main item,
-                  name: el.name,
-                  url: el.url,
-                  type: el.type,
-                  icon: el.icon,
-                  html: el.html,
-                  slug: el.slug,
-                  downloadNumber: el.downloadNumber,
-                  serviceDataCategoryId: el.serviceDataCategoryId,
-                };
-
-                return insertServiceDataListItem(customJsonEntry);
-              });
-
-              return Promise.all(insertPromises);
-            })
-            .then(() => {
-              console.log("All entries inserted successfully.");
-            })
-            .catch((error) => {
-              console.error("Error:", error);
-            });
-        }
-
-        //TODO now we handle serviceDataCategoryList
-
-        // //write to file
-        // fs.writeFile("output.json", JSON.stringify(allData), "utf8", (err) => {
-        //   if (err) {
-        //     console.error("Error writing file:", err);
-        //     return;
-        //   }
-        //   console.log("File has been saved successfully.");
-        // });
-      })
-      .catch((error) => {
-        console.error("Error retrieving data:", error);
-      });
-  })
-  .catch((error) => {
-    console.error("Error during query:", error);
-  });
-
 function insertServiceDataListItem(entry) {
   return new Promise((resolve, reject) => {
     const queryText = `
@@ -121,10 +52,130 @@ function insertServiceDataListItem(entry) {
         resolve(res);
       })
       .catch((err) => {
-        reject("Error executing insert query", err);
+        console.error(
+          `Error executing insert query for serviceDataListItem: ${err.message}`
+        );
+        reject(`Error executing insert query: ${err.message}`);
       });
   });
 }
+
+function insertServiceDataCategoryListItem(entry) {
+  return new Promise((resolve, reject) => {
+    const queryText = `
+        INSERT INTO servicedatacategorylistitem 
+        (id, name, dataitemid) 
+        VALUES ($1, $2, $3)
+      `;
+    const values = [entry.id, entry.name, entry.dataitemid];
+
+    client
+      .query(queryText, values)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        console.error(
+          `Error executing insert query for serviceDataCategoryListItem: ${err.message}`
+        );
+        reject(new Error(`Error executing insert query: ${err.message}`));
+      });
+  });
+}
+
+//? cant enforce foreign key contraint here, idk why
+
+clientConnection()
+  .then((client) => {
+    // Use the connected client for queries
+    return client.query("SELECT slug, id from maindataitem;");
+  })
+  .then((res) => {
+    console.log("Query result:", res.rows);
+    console.log(" " + res.rows.length);
+    return getDataItemPerSlug(res.rows);
+  })
+  .then((allData) => {
+    console.log("All data retrieved, its length is", allData.length);
+
+    const categoryPromises = allData.map((object) => {
+      const serviceDataCategoryList = object.serviceDataCategoryList || [];
+
+      return serviceDataCategoryList.map((el) => {
+        return getIdByTitle(object.name).then((result) => {
+          if (el.data.length === 0) {
+            return;
+          }
+          const customJsonEntry = {
+            id: el.data[0].serviceDataCategoryId,
+            name: el.name,
+            dataitemid: result[0].id,
+          };
+
+          const nestedServiceDataList = el.data || [];
+
+          const nestedInsertPromises = nestedServiceDataList.map((nestedEl) => {
+            const nestedEntry = {
+              dataitemid: result[0].id,
+              name: nestedEl.name,
+              url: nestedEl.url,
+              type: nestedEl.type,
+              icon: nestedEl.icon,
+              html: nestedEl.html,
+              slug: nestedEl.slug,
+              downloadNumber: nestedEl.downloadNumber,
+              serviceDataCategoryId: nestedEl.serviceDataCategoryId,
+            };
+            return insertServiceDataListItem(nestedEntry);
+          });
+
+          return Promise.all(nestedInsertPromises).then(() => {
+            if (customJsonEntry.id) {
+              return insertServiceDataCategoryListItem(customJsonEntry);
+            }
+          });
+        });
+      });
+    });
+
+    // Flatten the array of promises
+    const flattenedCategoryPromises = categoryPromises.flat();
+
+    return Promise.all(flattenedCategoryPromises).then(() => {
+      const dataPromises = allData.map((object) => {
+        const serviceDataList = object.serviceDataList || [];
+
+        return serviceDataList.map((el) => {
+          return getIdByTitle(object.name).then((result) => {
+            const customJsonEntry = {
+              dataitemid: result && result.length > 0 ? result[0].id : null,
+              name: el.name,
+              url: el.url,
+              type: el.type,
+              icon: el.icon,
+              html: el.html,
+              slug: el.slug,
+              downloadNumber: el.downloadNumber,
+              serviceDataCategoryId: el.serviceDataCategoryId || null,
+            };
+
+            return insertServiceDataListItem(customJsonEntry);
+          });
+        });
+      });
+
+      // Flatten the array of promises
+      const flattenedDataPromises = dataPromises.flat();
+
+      return Promise.all(flattenedDataPromises);
+    });
+  })
+  .then(() => {
+    console.log("All entries inserted successfully.");
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
 
 function getIdByTitle(title) {
   return new Promise((resolve, reject) => {
@@ -143,7 +194,8 @@ function getIdByTitle(title) {
         resolve(res.rows);
       })
       .catch((err) => {
-        reject("Error executing query", err);
+        console.error(`Error executing query for getIdByTitle: ${err.message}`);
+        reject(`Error executing query: ${err.message}`);
       });
   });
 }
@@ -161,7 +213,7 @@ async function getDataItemPerSlug(slugObjectArray) {
         const data = await fetchFromCustomUrl(slugObject.slug);
         console.log(`Data retrieved for slug ${slugObject.slug}`);
 
-        jsonobject = JSON.parse(data);
+        const jsonobject = JSON.parse(data);
         return jsonobject;
       } catch (error) {
         console.error(
@@ -180,30 +232,39 @@ async function getDataItemPerSlug(slugObjectArray) {
   }
 }
 
-function fetchFromCustomUrl(slug) {
+function fetchFromCustomUrl(slug, retries = 3) {
   return new Promise((resolve, reject) => {
-    let finalData = "";
-    https
-      .get(
-        {
-          hostname: "opendata.gov.al",
-          agent: false, // Create a new agent just for this one request,
-          path: "/api/sq/services/" + slug,
-        },
-        (res) => {
-          // Handle response data
-          res.on("data", (d) => {
-            finalData += d;
-          });
+    const attemptFetch = (attempt) => {
+      let finalData = "";
+      https
+        .get(
+          {
+            hostname: "opendata.gov.al",
+            agent: false, // Create a new agent just for this one request,
+            path: "/api/sq/services/" + slug,
+          },
+          (res) => {
+            // Handle response data
+            res.on("data", (d) => {
+              finalData += d;
+            });
 
-          // Resolve finalData when response ends
-          res.on("end", () => {
-            resolve(finalData);
-          });
-        }
-      )
-      .on("error", (e) => {
-        reject(e); // Reject promise on error
-      });
+            // Resolve finalData when response ends
+            res.on("end", () => {
+              resolve(finalData);
+            });
+          }
+        )
+        .on("error", (e) => {
+          if (attempt < retries) {
+            console.log(`Retrying fetch... attempt ${attempt + 1}`);
+            attemptFetch(attempt + 1); // Retry the fetch
+          } else {
+            reject(e); // Reject promise on error after all retries
+          }
+        });
+    };
+
+    attemptFetch(0); // Start with the first attempt
   });
 }
